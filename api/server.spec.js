21604d4c01bcc1490c7687d6781d6b3902afda12
Conary36 <cbeckford144@gmail.com>
2020-05-02 23:21:54 -0400

    testing files for hobbits and server.spec.js

---
+++ api/server.spec.js
@@ -0,0 +1,32 @@
+const request = require('supertest')
+const server = require('./server.js')
+
+describe('server', ()=>{
+    it('should set the testing environment', ()=>{
+        expect(process.env.DB_ENV).toBe('testing');
+    })
+    
+ describe('GET /', ()=>{
+     //RESULT CODE
+     it('should return zoo', ()=>{
+         return request(server).get('/')
+            .then(res=>{
+                expect(res.status).toBe(200)
+            })
+     })
+ })
+    it('should return 200 with await', async () => {
+        const res = await request(server).get('/')
+        expect(res.status).toBe(200)
+    })
+    
+    it('should return Json type', async () =>{
+        const res = await request(server).get('/')
+        expect(res.type).toBe('application/json')
+    })
+    
+    it('should return {api: "up"}', async ()=>{
+        const res = await request(server).get('/')
+        expect(res.body).toEqual({api: "up"})
+    })
+})
